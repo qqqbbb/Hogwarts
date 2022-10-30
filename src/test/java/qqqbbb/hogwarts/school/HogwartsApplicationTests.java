3217fa8438ada45a7618329ac4146a988f266281
qqqbbb <qqqinbox@ya.ru>
2022-10-30 10:23:10 +0300

    Initial commit

---
+++ src/test/java/qqqbbb/hogwarts/school/HogwartsApplicationTests.java
@@ -0,0 +1,15 @@
+package qqqbbb.hogwarts.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
